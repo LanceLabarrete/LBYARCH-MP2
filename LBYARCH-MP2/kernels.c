f08794b5346ae1cbe0bd60bf777fc41ebb36f3ac
Amelia Abenoja <97102877+ImAmeliaJoyce@users.noreply.github.com>
2024-04-01 09:08:07 +0800

    Finished Kernel in ASM

--- LBYARCH-MP2/kernels.c
+++ LBYARCH-MP2/kernels.c
@@ -10,21 +10,37 @@
 // for random number generation
 #include <time.h>
 
+void displayTableHeader() {
+	printf("---------------------------------------------\n");
+	printf("%5s | %15s | %15s\n", "Index", "Vector Z (C)", "Vector Z (x86-64)");
+	printf("---------------------------------------------\n");
+}
 
-float random_generator(float min, float max) {
+void displayTableBody(float* z, float* asmZ) {
+	for (int i = 0; i < 10; i++) {
+		printf("%5.0d | %15.6f | %15.6f\n", i + 1, z[i], asmZ[i]);
+	}
+}
+
+void displayTableFooter() {
+	printf("=============================================\n");
+}
+
+
+float random_float_generator(float min, float max) {
 	return ((float)rand() / (float)RAND_MAX) * (max - min) + min;
 }
 
 
 /***** SAXPY Operation *****/
-float saxpyC(int n, float a, float* x, float* y, float* z) {
+void saxpyC(int n, float a, float* x, float* y, float* z) {
 	for (int i = 0; i < n; i++) {
 		z[i] = a * x[i] + y[i];
 	}
 }
 
 
-void computeAveTime(double* time, int iterations, double* totalTime, double* averageTime) {
+void computeAveTime(double* time, int iterations, double* totalTime, double* averageTime, char mode) {
 	// Compute the total time
 	for (int i = 0; i < iterations; i++) {
 		*totalTime += time[i];
@@ -34,5 +50,12 @@ void computeAveTime(double* time, int iterations, double* totalTime, double* ave
 	*averageTime = *totalTime / iterations;
 
 	// Display the average time
-	printf("Average time: %f\n", *averageTime);
+	switch(mode) {
+		case 'C':
+			printf("Average time (for C)\t: %f\n", *averageTime);
+			break;
+		case 'A':
+			printf("Average time (for ASM)\t: %f\n", *averageTime);
+			break;
+	}
 }
--- LBYARCH-MP2/main.c
+++ LBYARCH-MP2/main.c
@@ -6,7 +6,7 @@
 * Version: 1.0
 *
 * References:
-* - Dynamic Array Allocation: https://www.geeksforgeeks.org/dynamic-array-in-c/
+* - Dynamic Array Allocation: https://www.geeksforgeeks.org/dynamic-memory-allocation-in-c-using-malloc-calloc-free-and-realloc/
 * - Random Number Generation: https://stackoverflow.com/questions/13408990/how-to-generate-random-float-number-in-c
 * - Time Measurement: https://www.geeksforgeeks.org/how-to-measure-time-taken-by-a-program-in-c/
 */
@@ -27,43 +27,38 @@
 
 
 // TO DO: Call the assembly function
-
+extern float saxpyASM(float scalarA, float x, float y);
 
 /***** Main Program *****/
 int main(int argc, char* argv[]) {
-	// Time measurement
-	clock_t startC, endC;
+	srand((unsigned int)time(NULL));
 
-	// Array to store the time measurements
-	double* timeC = (double*)malloc(ITERATIONS * sizeof(double));
+	// Time measurement for C
+	clock_t startC, endC;
+	double timeC[ITERATIONS];
 	double currTimeC = 0.0;
 	double totalTimeC = 0.0;
 	double averageTimeC = 0.0;
 
-
 	// Time measurement for assembly function
 	clock_t startASM, endASM;
-
-	// Array to store the time measurements
-	double* timeASM = (double*)malloc(ITERATIONS * sizeof(double));
+	double timeASM[ITERATIONS];
+	double currTimeASM = 0.0;
 	double totalTimeASM = 0.0;
 	double averageTimeASM = 0.0;
 
-
-	// Smallest single precision floating point number
-	//float min = (float)- 3.40282347E-38;
-	float min = (float)0.0;
-
-	// Largest single precision floating point number
-	//float max = (float)3.402823466e+38;
-	float max = (float)100.0;
+	// Single precision floating point number
+	//float min = (float)- 3.40282347E-38; 			// Smallest
+	float min = 0.0f;
+	//float max = (float)3.402823466e+38;			// Largest
+	float max = 100.0f;
 
 	// Vector sizes = 2^20, 2^24, 2^30
-	//int n[] = {1048576, 16777216, 1073741824};
+	//int n[3] = {1048576, 16777216, 1073741824};
 	int vectorN = 1048576;
 
 	// Scalar value
-	float scalarA = 0.0;
+	float scalarA = 0.0f;
 
 	// Vector values
 	float* x = (float*)malloc(vectorN * sizeof(float));
@@ -71,70 +66,77 @@ int main(int argc, char* argv[]) {
 	float* z = (float*)malloc(vectorN * sizeof(float));			// C Z vector	
 	float* asmZ = (float*)malloc(vectorN * sizeof(float));		// Assembly Z vector
 
-
-	// Main process
-	if (timeC != NULL && timeASM != NULL) {
-		for (int i = 0; i < ITERATIONS; i++) {	// Loop for the number of iterations
-
-			scalarA = random_generator(min, max);	// Set a random float number for the scalar value from min to max
-			printf("Scalar Value: %f\n", scalarA);
-
-			// Allocate memory for the vectors
-			if (x != NULL && y != NULL && z != NULL && asmZ != NULL) {
-
-				for (int i = 0; i < vectorN; i++) {	// Set a random float number for the X and Y vectors from min to max
-					x[i] = (float)random_generator(min, max);
-					y[i] = (float)random_generator(min, max);
-				}
-
-				/***** For C *****/
-				startC = clock();					// Start time measurement for C function
-				saxpyC(vectorN, scalarA, x, y, z);	// Call the C function
-				endC = clock();						// End time measurement for C function
-
-				// Calculate the time
-				currTimeC = (double)(endC - startC) / CLOCKS_PER_SEC;
-
-				// Print the first 10 values of the Z vectors
-				printf("Z Vector: ");
-				for (int i = 0; i < 10; i++) {
-					printf("%f ", z[i]);
-				}
-
-				printf("\n");
-
-				/***** For Assembly *****/
-				// TO DO: Implement the Assembly function and measure the time
+	// Temp variables
+	float tempX = 0.0f;
+	float tempY = 0.0f;
+	float tempZ = 0.0f;
 
 
+	if (x == NULL || y == NULL || z == NULL || asmZ == NULL) {
+		printf("Memory allocation failed.\n");
+		exit(0);
+	} else {
+		
+		// Main loop
+		for (int i = 0; i < ITERATIONS; i++) {				// Loop for the number of iterations
 
-				// Print the first 10 values of the asmZ vectors
-				// TO DO: Uncomment the code below
-				//printf("ASM Z Vector: ");
-				//for (int i = 0; i < 10; i++) {
-				//	printf("%f ", asmZ[i]);
-				//}
-			}
-			else {
-				printf("Memory allocation failed.\n");
+			printf("\nIteration: %d\n", i + 1);
+			scalarA = random_float_generator(min, max);		// Set a random float number for the scalar value from min to max
+
+			for (int i = 0; i < vectorN; i++) {				// Set a random float number for the X and Y vectors from min to max
+				x[i] = random_float_generator(min, max);
+				y[i] = random_float_generator(min, max);
 			}
 
+
+			/***** For Assembly *****/
+			startASM = clock();							// Start time measurement for Assembly function
+			
+			for (int i = 0; i < vectorN; i++) {
+				asmZ[i] = saxpyASM(scalarA, x[i], y[i]);
+			}
+			endASM = clock();							// End time measurement for Assembly function
+
+			// Calculate the time
+			currTimeASM = ((double)(endASM - startASM)) / CLOCKS_PER_SEC;
+
+
+			/***** For C *****/
+			startC = clock();					// Start time measurement for C function
+			saxpyC(vectorN, scalarA, x, y, z);	// Call the C function
+			endC = clock();						// End time measurement for C function
+
+			// Calculate the time
+			currTimeC = ((double)(endC - startC)) / CLOCKS_PER_SEC;
+
+			/***** Display Comparison *****/
+			displayTableHeader();
+			displayTableBody(z, asmZ);
+			displayTableFooter();
 
 			// Store the time measurement
 			timeC[i] = currTimeC;
+			timeASM[i] = currTimeASM;
 		}
+
+
+		// Display the n size
+		printf("\nVector Size\t\t: %d\n", vectorN);
+		printf("Scalar Value\t\t: %f\n", scalarA);
+
+		// Calculate the average time for the C function
+		computeAveTime(timeC, ITERATIONS, &totalTimeC, &averageTimeC, 'C');
+
+		// Calculate the average time for the assembly function
+		computeAveTime(timeASM, ITERATIONS, &totalTimeASM, &averageTimeASM, 'A');
+		printf("\n");
+
+		// Free the memory
+		free(x);
+		free(y);
+		free(z);
+		free(asmZ);
 	}
-	else {
-		printf("Memory allocation failed.\n");
-	}
-
-	// Calculate the average time for the C function
-	computeAveTime(timeC, ITERATIONS, &totalTimeC, &averageTimeC);
-
-	printf("\n");
-
-	// Calculate the average time for the assembly function
-	// TO DO: Call the computeAveTime function for the assembly function
 
 	return 0;
 }
